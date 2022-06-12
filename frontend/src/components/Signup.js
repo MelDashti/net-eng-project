3061994059e106598ca591cba20aa96420200637
Milad Dashti <72992585+MelDashti@users.noreply.github.com>
2022-06-12 22:28:32 +0430

    added a react component to display fetched data from database

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,21 +1,76 @@
+import React from "react";
+export default function Signup() {
+  const [formData, setFormData] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    joinNewsletter: false,
+  });
 
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === "checkbox" ? checked : value,
+    }));
+  }
 
-export default function Signup(){
-  
-  
-  
-  
-  return(
-    <div>
-      <input>
-      </input>
-      <input>
-      </input>
-      
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+    
+  }
 
+  return (
+    <div className="form-container" onSubmit={handleSubmit}>
+      <form>
+        <input
+          placeholder="First Name"
+          className="form--input"
+          required
+          onChange={handleChange}
+          name="firstName"
+          value={formData.firstName}
+        />
+        <input
+          placeholder="Last Name"
+          className="form--input"
+          required
+          onChange={handleChange}
+          name="lastName"
+          value={formData.lastName}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="form--input"
+          onChange={handleChange}
+          name="email"
+          value={formData.email}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          required
+          className="form--input"
+          onChange={handleChange}
+          name="password"
+          value={formData.password}
+        />
+        <div className="form--newsletter">
+          <input
+            id="okayToEmail"
+            type="checkbox"
+            checked={formData.joinNewsletter}
+            onChange={handleChange}
+            name="joinNewsletter"
+          />
+          <label htmlFor="okayToEmail">I want to join the newsletter</label>
+
+          <button className="form--submit">Submit</button>
+        </div>
+      </form>
     </div>
-  )
-  
-  
-
+  );
 }
